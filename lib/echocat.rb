1bb18d5f4aed1db44ce9f3400f8cf9f2fa1a8283
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2021-01-21 08:52:02 -0300

    Add welcome menu

--- lib/echocat.rb
+++ lib/echocat.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 module Echocat
-  def self.run
-    puts 'Hello World!'
+  class << self
+    def run
+      welcome
+    end
+
+    def welcome
+      puts 'Hello, welcome to the coolest library of books and magazines!'
+      puts 'But first, type your name.'
+
+      @name = $stdin.gets.chomp
+
+      puts "Hi #{@name}, glad to have you with us, choose one option"
+    end
   end
 end
